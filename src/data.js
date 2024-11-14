b44e545944ee91e1dfcf988ff77ecaac4810dac3
yatharth-7022 <yatharth7022@gmail.com>
2024-11-14 23:27:15 +0530

    changing data.js

--- src/data.js
+++ src/data.js
@@ -1,19 +1,19 @@
 const initialFriends = [
   {
     id: 118836,
-    name: "Clark",
+    name: "Yatharth",
     image: "https://i.pravatar.cc/48?u=118836",
     balance: -7,
   },
   {
     id: 933372,
-    name: "Sarah",
+    name: "Dhruv",
     image: "https://i.pravatar.cc/48?u=933372",
     balance: 20,
   },
   {
     id: 499476,
-    name: "Anthony",
+    name: "Devesh",
     image: "https://i.pravatar.cc/48?u=499476",
     balance: 0,
   },
